6096d099a8e2d1826f2ec667a7bfb7cbcec6c7fa
Isabel Lin <maoyolin.art@gmail.com>
2025-03-10 23:57:17 -0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,28 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
-const imagemin = require('gulp-imagemin')
-const uglify = require('gulp-uglify')
-const htmlmin = require('gulp-htmlmin')
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function scripts(){
     return gulp.src('./src/scripts/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./public/js'))
-}
-
-function images(){
-    return gulp.src('./src/images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./public/images'));
+    .pipe(gulp.dest('./dist/js'))
 }
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle:'compressed'}))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./dist/css'));
 }
 
-function html() {
-    return gulp.src('./src/*.html')
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('./public'))
+function images(){
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(html, styles, images, scripts)
-
-exports.watch = function () {
-    gulp.watch('./src/*.html', gulp.parallel(html))
+exports.default = gulp.parallel(styles,images,scripts);
+exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
-    gulp.watch('./src/images/*', gulp.parallel(images))
+    gulp.watch('./scr/scripts/*.js', gulp.parallel(scripts))
 }
