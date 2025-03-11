9330dbdd17282aa0ec71de59f4b3e26b8786fd3b
Isabel Lin <maoyolin.art@gmail.com>
2025-03-11 00:12:43 -0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
+
+function html() {
+    return gulp.src('./src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
+}
 
 function scripts(){
     return gulp.src('./src/scripts/*.js')
@@ -21,8 +28,10 @@ function images(){
     .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles,images,scripts);
+exports.default = gulp.parallel(html,styles,images,scripts);
 exports.watch = function(){
+    gulp.watch('./src/*.html', gulp.parallel(html))
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
     gulp.watch('./scr/scripts/*.js', gulp.parallel(scripts))
+    gulp.watch('./src/images/*', gulp.parallel(images))
 }
