300a55eb55053aab0f7b36dff1d3658f47a165d3
Isabel Lin <maoyolin.art@gmail.com>
2025-03-10 22:29:14 -0300

    Ajuste no arquivo gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,37 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
 
-function scripts(){
+function scripts() {
     return gulp.src('./src/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 }
 
-function images(){
+function images() {
     return gulp.src('./src/images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/images'));
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
 
-function styles(){
+function styles() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({outputStyle:'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'))
 }
 
-exports.default = gulp.parallel(styles,images,scripts);
-exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./scr/scripts/*.js', gulp.parallel(scripts))
+function html() {
+    return gulp.src('./src/*.html') 
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
 }
+
+exports.default = gulp.parallel(html, styles, images, scripts)
+
+exports.watch = function () {
+    gulp.watch('./src/*.html', gulp.parallel(html))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+    gulp.watch('./src/images/*', gulp.parallel(images))
+};
